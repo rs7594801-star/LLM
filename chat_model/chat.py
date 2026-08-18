7b089f821326c86a892651cd64fe417e11eb1e9d
Rachit Singh <rs7594801@gmail.com>
2026-08-19 00:48:23 +0530

    APT Trial or Simple impliment

--- chat_model/chat.py
+++ chat_model/chat.py
@@ -1,2 +1,22 @@
+# from dotenv import load_dotenv
+# load_dotenv()
+
+from langchain.chat_models import init_chat_model
+ 
+
+
 from dotenv import load_dotenv
+
 load_dotenv()
+
+# from langchain_groq import ChatGroq
+
+model = init_chat_model(
+    "openai/gpt-oss-20b" ,
+    model_provider="groq"
+)
+
+response = model.invoke("What is cricket?")
+
+print(response.content)
+# print(response)
