cd3938c49f9a22bad70bbf40b9f00b1a6fd0d188
Rachit Singh <rs7594801@gmail.com>
2026-08-26 23:25:34 +0530

    Updated RAG

---
+++ RAG_ret_aug_gen/main_rag.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+from langchain_mistralai import ChatMistralAI
+
+load_dotenv()
+
+model = ChatMistralAI(
+    model = "mistral-small-2506",
+    temperature = 0.9
+)
+result = model.invoke("Hello, how are you?")
+print(result.content)
