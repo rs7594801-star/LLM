cdca25f3084d93d9a21d0f016f9a14a1e88f03a3
Rachit Singh <rs7594801@gmail.com>
2026-08-31 20:09:29 +0530

    first try to create a RAG appli.

--- retrivers_RAG/main_rag.py
+++ retrivers_RAG/main_rag.py
@@ -1,20 +1,76 @@
 from dotenv import load_dotenv
+from langchain_community.vectorstores import Chroma
+# from langchain_openai import OpenAIEmbeddings
 from langchain_mistralai import ChatMistralAI
-from langchain_community.document_loaders import TextLoader
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_mistralai import MistralAIEmbeddings
+# from langchain_huggingface import HuggingFaceEmbeddings
 load_dotenv()
-data = TextLoader("Document_loaders/notes.txt")
-docs =data.load()
-template = ChatPromptTemplate.from_messages([
-    ("system","you are a AI that summarizea the text"),
-    ("human","{data}")
-])
 
-model = ChatMistralAI(
-    model = "mistral-small-2506",
-    temperature = 0.9
+from langchain_mistralai import MistralAIEmbeddings
+
+embedding_model = MistralAIEmbeddings(
+    model="mistral-embed"
 )
-prompt = template.format_messages(data = docs[0].page_content)
+vectorstore = Chroma(
+    persist_directory ="chroma_db_2",
+    embedding_function = embedding_model
 
-result = model.invoke(prompt)
-print(result.content)
+)
+
+
+retriever = vectorstore.as_retriever(
+    search_type ="mmr",
+    search_kwargs ={ 
+        "k" : 4 ,
+        "fetch_k" : 10 ,
+        "lambda_mult" : 0.5}
+    
+)
+
+llm = ChatMistralAI(model="mistral-small-latest")
+prompt = ChatPromptTemplate.from_messages(
+    [
+    ("system",
+        """you are a helpful ai assistant ,
+    Use only provided context to answere the question .,
+    if the answere is not present in the context ,
+    say : "i could not find the ans int the document " """) ,
+    (
+        "human" ,
+        """Context:
+        {context}
+        
+        Question :
+        {question}
+        
+        """
+
+      
+
+    )
+    ]
+)
+
+print("rag system created ")
+
+print("press exit to exit")
+
+while True:
+    query = input("you :")
+    if query.lower() == "exit" :
+        print("Rag system Closed .") 
+        break 
+    docs = retriever.invoke(query)
+
+    context = "\n\n".join(
+        [doc.page_content for doc in docs]
+    )
+    final_prompt = prompt.invoke({
+        "context": context,
+        "question": query
+    })
+    response = llm.invoke(final_prompt)
+
+    print(f"\nAI: {response.content}")
+# print("You asked:",query)
